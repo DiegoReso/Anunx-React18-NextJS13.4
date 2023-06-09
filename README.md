e5bb91971d0bb50ab49cc7dd82df8736d7925b32
Diego Reis <diegoreso@me.com>
2023-06-09 09:23:26 -0300

    formik installation and configuration, title validation

--- README.md
+++ README.md
@@ -2,7 +2,8 @@ NEXT JS
 Material UI
 React Dropzone
 React-material-ui-carousel
-
+Formik
+Yup
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
--- app/user/publish/page.js
+++ app/user/publish/page.js
@@ -7,6 +7,18 @@ import { DeleteForever } from "@mui/icons-material"
 import {useDropzone} from 'react-dropzone'
 import { useState } from "react"
 
+import { Formik } from "formik"
+import * as yup from 'yup'
+
+
+const validationSchema = yup.object().shape({
+  title: yup.string()
+    .min(6, 'Escreva um título maior')
+    .max(100,'Título muito grande')
+    .required('Campo Obrigatório'),
+})
+
+
 
 const Publish =()=>{
 
@@ -41,303 +53,334 @@ const Publish =()=>{
 
 
     <TemplateDefault>
-      <Container maxWidth="sm">
-        <Typography 
-        component="h1" 
-        variant="h2" 
-        align="center"
-        sx={{mt: '50px'}}>
-          Publicar Anúncio
-        </Typography>
-
-        <Typography 
-        component="h5" 
-        variant="h5" 
-        align="center"
-        sx={{mt: '5px', mb: '20px'}}>
-          Quanto mais detalhado melhor
-        </Typography>
-      </Container>
-
-      <Container maxWidth="md">
-        <Box sx={{bgcolor:'white',borderRadius: '12px', p:'10px'}}>
-        <Typography 
-        component="h6" 
-        variant="h6" 
-        >
-          Título do anúncio
-        </Typography>
-        <TextField
-          label="ex.: Bicicleta Aro 18 com garantia"
-          size="small"
-          fullWidth
-          />
-
-        <Typography 
-        component="h6" 
-        variant="h6"
-        sx={{mt: '15px'}}
-        >
-          Categoria
-        </Typography>
-        <Select
-        native
-        variant="outlined"
-        value=""
-        fullWidth
-        onChange={()=> {}}
-        inputProps={{
-          name: 'age',
+      <Formik
+        initialValues={{
+          title: ''
         }}
-        >
-          
-          <option value="">Selecione</option>
-          <option value={1}>Bebe e Criança</option>
-          <option value={2}>Agricultura</option>
-          <option value={3}>Moda</option>
-          <option value={4}>CArros, Motos e Barcos</option>
-          <option value={5}>Serviços</option>
-          <option value={6}>Lazer</option>
-          <option value={7}>Animais</option>
-          <option value={8}>Moveis, Casa e Jardim</option>
-          <option value={9}>Imóveis</option>
-          <option value={10}>Equipamentos e ferramentas</option>
-          <option value={11}>Celulares e Tablets</option>
-          <option value={12}>Esporte</option>
-          <option value={13}>Tecnologia</option>
-          <option value={14}>Emprego</option>
-          <option value={15}>Outros</option>
+        validationSchema={validationSchema}
+        onSubmit={(values)=>{
+          console.log('Ok enviou', values)
+        }}
+      >
+        {
+          ({
+            values,
+            errors,
+            handleChange,
+            handleSubmit,
+          })=>{
+            console.log(errors)
+            return(
+              <form onSubmit={handleSubmit}>
+                <Container maxWidth="sm">
+                  <Typography 
+                  component="h1" 
+                  variant="h2" 
+                  align="center"
+                  sx={{mt: '50px'}}>
+                    Publicar Anúncio
+                  </Typography>
+
+                  <Typography 
+                  component="h5" 
+                  variant="h5" 
+                  align="center"
+                  sx={{mt: '5px', mb: '20px'}}>
+                    Quanto mais detalhado melhor
+                  </Typography>
+                </Container>
+
+                <Container maxWidth="md">
+                  <Box sx={{bgcolor:'white',borderRadius: '12px', p:'10px'}}>
+                  <Typography 
+                  component="h6" 
+                  variant="h6" 
+                  >
+                    Título do anúncio
+                  </Typography>
+                  <TextField
+                    name="title"
+                    value={values.title}
+                    onChange={handleChange}
+                    label="ex.: Bicicleta Aro 18 com garantia"
+                    size="small"
+                    fullWidth
+                    error={errors.title}
+                    helperText={errors.title}
+                    />
+
+                  <Typography 
+                  component="h6" 
+                  variant="h6"
+                  sx={{mt: '15px'}}
+                  >
+                    Categoria
+                  </Typography>
+                  <Select
+                  native
+                  variant="outlined"
+                  value=""
+                  fullWidth
+                  onChange={()=> {}}
+                  inputProps={{
+                    name: 'age',
+                  }}
+                  >
+                    
+                    <option value="">Selecione</option>
+                    <option value={1}>Bebe e Criança</option>
+                    <option value={2}>Agricultura</option>
+                    <option value={3}>Moda</option>
+                    <option value={4}>CArros, Motos e Barcos</option>
+                    <option value={5}>Serviços</option>
+                    <option value={6}>Lazer</option>
+                    <option value={7}>Animais</option>
+                    <option value={8}>Moveis, Casa e Jardim</option>
+                    <option value={9}>Imóveis</option>
+                    <option value={10}>Equipamentos e ferramentas</option>
+                    <option value={11}>Celulares e Tablets</option>
+                    <option value={12}>Esporte</option>
+                    <option value={13}>Tecnologia</option>
+                    <option value={14}>Emprego</option>
+                    <option value={15}>Outros</option>
 
 
 
-        </Select>
+                  </Select>
 
-        </Box>
+                  </Box>
 
 
-      </Container>
-      
-      <Container
-       maxWidth="md"
-       sx={{mt: '25px'}}
-       >
-        <Box 
-          sx={{bgcolor:'white',
-          borderRadius: '12px', 
-          p:'10px'
-          }}>
-          <Typography 
-          component="h6" 
-          variant="h6" 
-          >
-            Imagens
-          </Typography>
-          <Typography component="h7" variant="h7">
-            A primeira imagem é a foto principal do anúncio
-          </Typography>
+                </Container>
+                
+                <Container
+                maxWidth="md"
+                sx={{mt: '25px'}}
+                >
+                  <Box 
+                    sx={{bgcolor:'white',
+                    borderRadius: '12px', 
+                    p:'10px'
+                    }}>
+                    <Typography 
+                    component="h6" 
+                    variant="h6" 
+                    >
+                      Imagens
+                    </Typography>
+                    <Typography component="h7" variant="h7">
+                      A primeira imagem é a foto principal do anúncio
+                    </Typography>
 
-          <Box 
-            sx={{
-              display: 'flex',
-              gap : 1,
-              flexWrap: 'wrap',
-              ml: '15px'
-          }}>
-            <Box 
-            {...getRootProps()}
+                    <Box 
+                      sx={{
+                        display: 'flex',
+                        gap : 1,
+                        flexWrap: 'wrap',
+                        ml: '15px'
+                    }}>
+                      <Box 
+                      {...getRootProps()}
 
-            sx={{
-              display:'flex',
-              justifyContent: 'center',
-              alignItems: 'Center',
-              mt: '10px',
-              border: '2px dashed grey',
-              width: 150,
-              height: 150,
-              bgcolor: '#e0e0e0'
-              
-            }}>
-              <input {...getInputProps()} />
-              <Typography
-               align="center" 
-               variant="body2"
-              
-               >
-                Clique ou arraste a imagem aqui.
-              </Typography>
-
-            </Box>
-
-              {
-
-                files.map((file , index)=> (
-                  
-                <Box
-                  key={file.name}
-                  sx={{
-                    display:'flex',
-                    justifyContent: 'center',
-                    alignItems: 'Center',
-                    mt: '10px',
-                    border: '2px dashed grey',
-                    width: 150,
-                    height: 150,
-                    bgcolor: '#e0e0e0',
-                    backgroundSize: 'Cover',
-                    backgroundImage: `url(${file.preview})`,
-                      
-                   
-                  }}>
-                    <Box
                       sx={{
                         display:'flex',
-                        position: 'relative',
                         justifyContent: 'center',
                         alignItems: 'Center',
+                        mt: '10px',
+                        border: '2px dashed grey',
                         width: 150,
                         height: 150,
-                        "&:hover": {
-                          backgroundColor: 'rgba(0, 0, 0, 0.7)'
-                        },
-                      }}
-                     >
-                      
-                      <IconButton onClick={() => handleRemoveFile(file.name)}>
-                        <DeleteForever fontSize="large" color="secondary"/>
-                      </IconButton>
-
-                      {
-
-                        index === 0 ?
-                        <Box 
-                        sx={{
-                          position: 'absolute',
-                          left: 0,
-                          bottom:0,
-                          backgroundColor: 'blue',
-                        }}
-                      >
-                        <Typography variant="body2" color="secondary">
-                          Principal
+                        bgcolor: '#e0e0e0'
+                        
+                      }}>
+                        <input {...getInputProps()} />
+                        <Typography
+                        align="center" 
+                        variant="body2"
+                        
+                        >
+                          Clique ou arraste a imagem aqui.
                         </Typography>
+
                       </Box>
 
-                      : null
+                        {
+
+                          files.map((file , index)=> (
+                            
+                          <Box
+                            key={file.name}
+                            sx={{
+                              display:'flex',
+                              justifyContent: 'center',
+                              alignItems: 'Center',
+                              mt: '10px',
+                              border: '2px dashed grey',
+                              width: 150,
+                              height: 150,
+                              bgcolor: '#e0e0e0',
+                              backgroundSize: 'Cover',
+                              backgroundImage: `url(${file.preview})`,
+                                
+                            
+                            }}>
+                              <Box
+                                sx={{
+                                  display:'flex',
+                                  position: 'relative',
+                                  justifyContent: 'center',
+                                  alignItems: 'Center',
+                                  width: 150,
+                                  height: 150,
+                                  "&:hover": {
+                                    backgroundColor: 'rgba(0, 0, 0, 0.7)'
+                                  },
+                                }}
+                              >
+                                
+                                <IconButton onClick={() => handleRemoveFile(file.name)}>
+                                  <DeleteForever fontSize="large" color="secondary"/>
+                                </IconButton>
+
+                                {
+
+                                  index === 0 ?
+                                  <Box 
+                                  sx={{
+                                    position: 'absolute',
+                                    left: 0,
+                                    bottom:0,
+                                    backgroundColor: 'blue',
+                                  }}
+                                >
+                                  <Typography variant="body2" color="secondary">
+                                    Principal
+                                  </Typography>
+                                </Box>
+
+                                : null
 
 
-                      }
-                     
-                    </Box>
+                                }
+                              
+                              </Box>
+                              
+                          </Box>
+
+
+                          ))
+                        }
+
                     
-                </Box>
+                    </Box>  
+                  </Box>
+                </Container>
+                
+                <Container
+                maxWidth="md"
+                sx={{mt: '25px'}}
+                >
+                  <Box sx={{bgcolor:'white',borderRadius: '12px', p:'10px'}}>
+                  <Typography 
+                  component="h6" 
+                  variant="h6" 
+                  sx={{mt: '10px'}}>
+                    Escreva os detalhes do item que esta vendendo
+                  </Typography>
+                  <TextField
+                    multiline
+                    rows={6}
+                    variant="outlined"
+                    fullWidth
+                  />
+                    
+                  </Box>
+                </Container>
 
-
-                ))
-              }
-
-           
-          </Box>  
-        </Box>
-      </Container>
-      
-      <Container
-       maxWidth="md"
-       sx={{mt: '25px'}}
-       >
-        <Box sx={{bgcolor:'white',borderRadius: '12px', p:'10px'}}>
-        <Typography 
-        component="h6" 
-        variant="h6" 
-        sx={{mt: '10px'}}>
-          Escreva os detalhes do item que esta vendendo
-        </Typography>
-        <TextField
-          multiline
-          rows={6}
-          variant="outlined"
-          fullWidth
-        />
-          
-        </Box>
-      </Container>
-
-    <Container
-       maxWidth="md"
-       sx={{mt: '25px'}}
-       >
-        <Box sx={{bgcolor:'white',borderRadius: '12px', p:'10px'}}>
-          <Typography 
-          component="h6" 
-          variant="h6" 
-          >
-            Preço
-          </Typography>
-          <br />
-            <FormControl fullWidth variant="outlined">
+              <Container
+                maxWidth="md"
+                sx={{mt: '25px'}}
+                >
+                  <Box sx={{bgcolor:'white',borderRadius: '12px', p:'10px'}}>
+                    <Typography 
+                    component="h6" 
+                    variant="h6" 
+                    >
+                      Preço
+                    </Typography>
+                    <br />
+                      <FormControl fullWidth variant="outlined">
+                        
+                        <OutlinedInput 
+                          onChange={()=>{}}
+                          startAdornment ={<InputAdornment position="start">R$</InputAdornment>}
+                          labelWidth={40}
+                    
+                      />
+                        
+                      
+                    </FormControl>
+                  </Box>
+                </Container>
               
-              <OutlinedInput 
-                onChange={()=>{}}
-                startAdornment ={<InputAdornment position="start">R$</InputAdornment>}
-                labelWidth={40}
-          
-            />
-              
-            
-          </FormControl>
-        </Box>
-      </Container>
-     
 
-      
-      <Container
-       maxWidth="md"
-       sx={{mt: '25px'}}
-       >
-        <Box sx={{bgcolor:'white',borderRadius: '12px', p:'10px'}}>
-        <Typography 
-        component="h6" 
-        variant="h6" 
-        >
-          Dados de contato
-        </Typography>
-        <TextField
-          sx={{mt: '5px'}}
-          label="Nome"
-          size="small"
-          variant="outlined"
-          fullWidth
-        />
+                
+                <Container
+                maxWidth="md"
+                sx={{mt: '25px'}}
+                >
+                  <Box sx={{bgcolor:'white',borderRadius: '12px', p:'10px'}}>
+                  <Typography 
+                  component="h6" 
+                  variant="h6" 
+                  >
+                    Dados de contato
+                  </Typography>
+                  <TextField
+                    sx={{mt: '5px'}}
+                    label="Nome"
+                    size="small"
+                    variant="outlined"
+                    fullWidth
+                  />
 
-          <TextField
-          sx={{mt: '5px'}}
-          label="Email"
-          size="small"
-          variant="outlined"
-          fullWidth
-        />
-          <TextField
-          sx={{mt: '5px'}}
-          label="Telefone"
-          size="small"
-          variant="outlined"
-          fullWidth
-        />
-        </Box>
+                    <TextField
+                    sx={{mt: '5px'}}
+                    label="Email"
+                    size="small"
+                    variant="outlined"
+                    fullWidth
+                  />
+                    <TextField
+                    sx={{mt: '5px'}}
+                    label="Telefone"
+                    size="small"
+                    variant="outlined"
+                    fullWidth
+                  />
+                  </Box>
 
-      </Container>
+                </Container>
 
-      
-      <Container
-      sx={{mt: '10px'}}
-       maxWidth="md">
-          <Box textAlign=
-          "right">
-            <Button variant="contained">
-              Anunciar
-            </Button>
-          </Box>
+                
+                <Container
+                  sx={{mt: '10px'}}
+                  maxWidth="md">
+                    <Box textAlign=
+                    "right">
+                      <Button type="submit" variant="contained">
+                        Anunciar
+                      </Button>
+                    </Box>
 
-      </Container> 
+                </Container> 
+                        
+            </form>
+                      )
+                    }
+                  }
+                </Formik>
+                
 
     </TemplateDefault>
 
